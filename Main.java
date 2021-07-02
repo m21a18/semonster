d7de24c83422f40526bfc936cdb81874c3b21212
m21a18 <m1m21a18@oit.ac.jp>
2021-07-03 00:16:09 +0900

    設定ファイルをまとめてコミット

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
+}
+public class Player {
+  ArrayList<Monster> deck = new ArrayList<>();
+
+  public void drawMonsters() {
+    for (int i = 0; i < 5; i++) {
+      this.deck.add(new Monster());
+    }
+  }
+
+  public void showDeck() {
+    for (Monster m : this.deck) {
+      System.out.println(m);
+    }
+  }
+
 }
